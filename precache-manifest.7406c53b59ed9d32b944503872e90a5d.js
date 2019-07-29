56c4722647bc469ad53e4fdf05913d5049294a79
willa75 <anthonywi99@yahoo.com>
2019-07-29 13:43:47 -0400

    Updates

---
+++ precache-manifest.7406c53b59ed9d32b944503872e90a5d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14b57916dd36f922ab3777cb77908423",
+    "url": "/kitty-cards/index.html"
+  },
+  {
+    "revision": "21d95864e10847fb9c62",
+    "url": "/kitty-cards/static/css/main.fa9e8c50.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/kitty-cards/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "21d95864e10847fb9c62",
+    "url": "/kitty-cards/static/js/main.16560768.chunk.js"
+  },
+  {
+    "revision": "e76a0fd6547c95dce96b",
+    "url": "/kitty-cards/static/js/runtime~main.05ef5e29.js"
+  }
+]);
